2a2f5675bac78bad73a85172e326575ca2ae5a72
Nitesh <knk77524@gmail.com>
2025-05-20 02:32:22 +0530

    update server.js

--- server.js
+++ server.js
@@ -4,35 +4,40 @@ const colors = require("colors");
 const morgan = require("morgan");
 const cors = require("cors");
 const connectDB = require("./config/db");
-//dot config
+
+// dot config
 dotenv.config();
 
-//mongodb connection
+// mongodb connection
 connectDB();
 
-//rest object
-const app = express(); 
+// rest object
+const app = express();
 
-//middlewares
+// middlewares
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
 
-//routes
-// 1 test route
+// default root route ✅
+app.get("/", (req, res) => {
+  res.send("Welcome to the Blood Bank Management API");
+});
+
+// routes
 app.use("/api/v1/test", require("./routes/testRoutes"));
 app.use("/api/v1/auth", require("./routes/authRoutes"));
 app.use("/api/v1/inventory", require("./routes/inventoryRoutes"));
 app.use("/api/v1/analytics", require("./routes/analyticsRoutes"));
 app.use("/api/v1/admin", require("./routes/adminRoutes"));
 
-//port
+// port
 const PORT = process.env.PORT || 8080;
 
-//listen
+// listen
 app.listen(PORT, () => {
   console.log(
-    `Node Server Running In ${process.env.DEV_MODE} ModeOn Port ${process.env.PORT}`
+    `Node Server Running In ${process.env.DEV_MODE} Mode On Port ${PORT}`
       .bgBlue.white
   );
 });
